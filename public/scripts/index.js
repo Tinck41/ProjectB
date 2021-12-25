9d5fb5461b918f7eded630aa631a15280b232319
Dmitry Mukhin <dmitry.mukhin@mytona.com>
2021-12-25 12:43:24 +0900

    firebase final

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -14,31 +14,18 @@ const firebaseApp = initializeApp({
 let db = getFirestore();
 
 let modal = document.getElementById('agreement-modal')
-
-let tariffBtn1 = document.getElementById('tariff-btn-1')
-let tariffBtn2 = document.getElementById('tariff-btn-2')
-let tariffBtn3 = document.getElementById('tariff-btn-3')
 let chosenTariff = -1
-// let formBtn = document.getElementById('form-button')
-let closeBtn = document.getElementById("close-btn")
+
+const tariffBtn1 = document.getElementById('tariff-btn-1')
+const tariffBtn2 = document.getElementById('tariff-btn-2')
+const tariffBtn3 = document.getElementById('tariff-btn-3')
+const formBtn = document.getElementById('form-button')
+const closeBtn = document.getElementById("close-btn")
+const whatsAppBtn = document.getElementById('whatsapp-btn')
 
 tariffBtn1.onclick = async () => {
     modal.style .display = "block"
     chosenTariff = 0
-
-    try {
-        const docRef = await addDoc(collection(db, 'banking'), {
-            first_name: "test",
-            last_name: "test",
-            father_name: "test",
-            birth_date: "test",
-            address: "test",
-            tariff: chosenTariff
-        })
-        console.log('Document written with ID: ', docRef.id)
-    } catch(e) {
-        console.error('Error adding document: ', error)
-    }
 }
 
 tariffBtn2.onclick = () => {
@@ -49,6 +36,49 @@ tariffBtn2.onclick = () => {
 tariffBtn3.onclick = () => {
     modal.style .display = "block"
     chosenTariff = 2
+}
+
+formBtn.onclick = async () => {
+    const firstName = document.getElementById('first-name').value
+    const lastName = document.getElementById('last-name').value
+    const fatherName = document.getElementById('father-name').value
+    const birthDay = document.getElementById('birth-day').value
+    const address = document.getElementById('address').value
+    console.log('Form button is pressed')
+
+    try {
+        const docRef = await addDoc(collection(db, 'banking'), {
+            first_name: firstName,
+            last_name: lastName,
+            father_name: fatherName,
+            birth_date: birthDay,
+            address: address,
+            tariff: chosenTariff
+        })
+        console.log('Document written with ID: ', docRef.id)
+        formBtn.innerText = 'Выполнено'
+        formBtn.disabled = true
+        formBtn.style.backgroundColor = '#bbbbbb'
+    } catch(e) {
+        console.log('Error adding document: ', error)
+    }
+}
+
+whatsAppBtn.onclick = async () => {
+    const phone = document.getElementById('phone-number').value
+    console.log('WhatsApp button is pressed')
+    try {
+        const docRef = await addDoc(collection(db, 'whatsapp'), {
+            phone_number: phone,
+        })
+        console.log('Document written with ID: ', docRef.id)
+        whatsAppBtn.innerText = 'Выполнено'
+        whatsAppBtn.disabled = true
+        whatsAppBtn.style.backgroundColor = '#bbbbbb'
+
+    } catch(e) {
+        console.log('Error adding document: ', error)
+    }
 }
 
 closeBtn.onclick = () => {
